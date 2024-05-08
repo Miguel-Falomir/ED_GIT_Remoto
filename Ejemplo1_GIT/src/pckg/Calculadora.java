5301138c0fe8db216ac2adff50bf1fa115b92b7f
alumno <alumno@pc15>
2024-05-08 12:39:15 +0200

    1º commit

---
+++ Ejemplo1_GIT/src/pckg/Calculadora.java
@@ -0,0 +1,48 @@
+package pckg;
+
+public class Calculadora {
+	// sumar
+	public double add(double number1, double number2) {
+		return number1 + number2;
+	}
+
+	// restar
+	public double subtract(double number1, double number2) {
+		return number1 - number2;
+	}
+
+	// multiplicar
+	public double multiply(double number1, double number2) {
+		return number1 * number2;
+	}
+
+	// dividir
+	public double divide(double number1, double number2) {
+		return number1 / number2;
+	}
+
+	// comparar
+	public boolean greaterThan(double number1, double number2) {
+		if (number1 > number2) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean lowerThan(double number1, double number2) {
+		if (number1 < number2) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean equalTo(double number1, double number2) {
+		if (number1 == number2) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
