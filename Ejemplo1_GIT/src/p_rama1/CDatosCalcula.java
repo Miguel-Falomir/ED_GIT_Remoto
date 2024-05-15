573548752c0042350b40634a1569b0d39e5a9566
alumno <alumno@pc15>
2024-05-15 12:42:18 +0200

    Create & set 'rama2'

---
+++ Ejemplo1_GIT/src/p_rama1/CDatosCalcula.java
@@ -0,0 +1,51 @@
+package p_rama1;
+
+public class CDatosCalcula {
+	private Integer num1;
+	private Integer num2;
+	private Integer num3;
+	private Integer resultado;
+	
+	public CDatosCalcula() {
+		
+	}
+	public CDatosCalcula(	Integer num1,
+							Integer num2,
+							Integer num3,
+							Integer resultado
+							){
+		this.num1 = num1;
+		this.num2 = num2;
+		this.num3 = num3;
+		this.resultado = resultado;
+	}
+
+	public Integer getNum1() {
+		return num1;
+	}
+	public void setNum1(Integer num1) {
+		this.num1 = num1;
+	}
+
+	public Integer getNum2() {
+		return num2;
+	}
+	public void setNum2(Integer num2) {
+		this.num2 = num2;
+	}
+
+	public Integer getNum3() {
+		return num3;
+	}
+	public void setNum3(Integer num3) {
+		this.num3 = num3;
+	}
+
+	public Integer getResultado() {
+		return resultado;
+	}
+	public void setResultado(Integer resultado) {
+		this.resultado = resultado;
+	}
+	
+}
